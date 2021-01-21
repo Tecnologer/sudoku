81329a8bf2dbe14125200f036f214bf589f61240
Rey David <rdominguez@tecnologer.net>
2021-01-20 23:35:36 -0600

    cli: implement commands (wip)

--- clients/cli/main.go
+++ clients/cli/main.go
@@ -3,70 +3,68 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 
-	sudoku "github.com/tecnologer/sudoku/src"
+	"github.com/tecnologer/sudoku/clients/cli/cmd"
 )
 
 var (
-	reqVersion = flag.Bool("version", false, "returns the current version")
+	verbouse   = flag.Bool("v", false, "verbouse")
 	version    string
 	minversion string
 )
 
-func init() {
-	flag.Parse()
-}
-
 func main() {
-	if *reqVersion {
-		fmt.Printf("%s.%s\n", version, minversion)
+	flag.Parse()
+
+	if len(os.Args) < 2 {
+		printCmds()
 		return
 	}
+
+	if os.Args[1] == "version" {
+		fmt.Printf("%s%s\n", version, minversion)
+		return
+	}
+
+	if os.Args[1] != "new" {
+		printCmds()
+		return
+	}
+
+	for {
+		cmd.CallCmd()
+	}
+
 	// defer profile.Start(profile.CPUProfile, profile.ProfilePath(".")).Stop()
 	// defer profile.Start(profile.MemProfile, profile.MemProfileRate(1), profile.ProfilePath(".")).Stop()
-	l := sudoku.MasterLevel
-	fmt.Printf("creating new game as %s\n", l)
-	game := sudoku.NewGame(l)
-	printGame(game)
+	// l := sudoku.MasterLevel
+	// fmt.Printf("creating new game as %s\n", l)
+	// game := sudoku.NewGame(l)
+	// printGame(game)
 
-	game.Solve()
-	fmt.Println("solved")
-	printGame(game)
+	// game.Solve()
+	// fmt.Println("solved")
+	// printGame(game)
 
-	err := game.Validate()
-	fmt.Println("validated")
+	// err := game.Validate()
+	// fmt.Println("validated")
 
-	if err.Count > 0 {
-		for t, errs := range err.Errs {
-			for _, e := range errs {
-				fmt.Printf("Error type:%s at (%d,%d)\n", t, e.X, e.Y)
-			}
-		}
-		return
-	}
+	// if err.Count > 0 {
+	// 	for t, errs := range err.Errs {
+	// 		for _, e := range errs {
+	// 			fmt.Printf("Error type:%s at (%d,%d)\n", t, e.X, e.Y)
+	// 		}
+	// 	}
+	// 	return
+	// }
 
-	fmt.Println("you won!")
+	// fmt.Println("you won!")
 }
 
-func printGame(game *sudoku.Game) {
-	for x := 0; x < 9; x++ {
-		fmt.Print("|")
-		for y := 0; y < 9; y++ {
-			if game.Board[x][y] != 0 {
-				fmt.Print(game.Board[x][y])
-			} else {
-				fmt.Print("-")
-			}
-			fmt.Print(" ")
-
-			if (y+1)%3 == 0 {
-				fmt.Print("|")
-			}
-		}
-		if (x+1)%3 == 0 && x < 8 {
-			fmt.Println("\n|------|------|------|")
-		} else {
-			fmt.Println()
-		}
-	}
+func printCmds() {
+	fmt.Println("Sudoku-CLI provides the following commands to start")
+	fmt.Println()
+	fmt.Println("  new", "\n\tcreates a new game")
+	fmt.Println("  version", "\n\treturns the current version")
 }
