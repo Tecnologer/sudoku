7a5a728c60931807f1f7eae92cf8fde43fe45f8a
Rey David <rdominguez@tecnologer.net>
2021-01-23 15:21:42 -0600

    cli.command: search command by name or alias

--- clients/cli/cmd/command.go
+++ clients/cli/cmd/command.go
@@ -19,3 +19,21 @@ func (c *command) String() string {
 	}
 	return fmt.Sprintf("- %s: %s%s", c.cmd, c.about, alias)
 }
+
+func (c *command) isAlias(alias string) bool {
+	for _, a := range c.alias {
+		if a == alias {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (c *command) isCommand(cmd string) bool {
+	if c.cmd == cmd {
+		return true
+	}
+
+	return c.isAlias(cmd)
+}
