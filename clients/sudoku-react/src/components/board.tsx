c6d1d643b2f5f49f151f9305d3db82026fb39160
Rey David <rdominguez@tecnologer.net>
2021-01-24 17:34:25 -0600

    display board, inputs (WIP)

--- clients/sudoku-react/src/components/board.tsx
+++ clients/sudoku-react/src/components/board.tsx
@@ -1,40 +1,130 @@
-import * as React from "react";
+import React, { ChangeEvent } from "react";
 import "../App.css";
 
+interface ICoordinate {
+  x: number;
+  y: number;
+  val: string;
+  isLocked: boolean;
+}
 export interface BoardProps {
-  content: number[][];
+  content: string[][];
 }
 
 export interface BoardState {
-  content: number[][];
+  content: ICoordinate[][];
 }
 
 class Board extends React.Component<BoardProps, BoardState> {
-  state = { content: [] };
+  componentWillReceiveProps(nextProps: BoardProps) {
+    this.setState({
+      content: this.parseContentToCoordinates(nextProps.content),
+    });
+  }
+
+  state = { content: Array<ICoordinate[]>() };
+  coordinates = Array<ICoordinate[]>();
 
   render() {
     const { content } = this.props;
-    return <div className="board">{this.getCoordinateFields(content)}</div>;
+    this.coordinates = this.parseContentToCoordinates(content);
+    return (
+      <div className="board">{this.getCoordinateFields(this.coordinates)}</div>
+    );
   }
 
-  getCoordinateFields = (content: number[][]) => {
+  componentDidMount() {
+    this.setState({ content: this.coordinates });
+  }
+
+  getCoordinateFields = (content: ICoordinate[][]) => {
     return content.map((row, x) => {
       return (
-        <div>
+        <div id={"row_" + x}>
           {row.map((col, y) => {
-            return <div className="coordinate">{this.formatCell(col)}</div>;
+            return (
+              <div
+                id={"col_" + x + "_" + y}
+                className={this.getCoordinateClass(x, y)}
+              >
+                {this.formatCell(col)}
+              </div>
+            );
           })}
         </div>
       );
     });
   };
 
-  formatCell = (val: number) => {
-    if (val > 0) {
-      return <span>{val}</span>;
+  formatCell = (coor: ICoordinate) => {
+    if (coor.isLocked) {
+      return <span id={"cell_" + coor.x + "_" + coor.y}>{coor.val}</span>;
     }
 
-    return <input type="text" className="coordinate-editable " />;
+    return (
+      <input
+        id={coor.x + "," + coor.y}
+        type="text"
+        className="coordinate-editable "
+        value={coor.val}
+        onChange={this.handleChange}
+      />
+    );
+  };
+
+  getCoordinateClass = (x: number, y: number) => {
+    let c: string = "coordinate";
+
+    if (y === 0) {
+      c += " coordinate-start";
+    } else if (y === 8) {
+      c += " coordinate-end";
+    } else {
+      c += " coordinate-middle";
+    }
+
+    if (x === 8) {
+      c += " coordinate-end-row";
+    }
+
+    if ((x + 1) % 3 === 0) {
+      c += " coordinate-bolder-row";
+    }
+    // console.log(x, y, x % 3, y % 3);
+    if ((y + 1) % 3 === 0) {
+      c += " coordinate-bolder-column";
+    }
+
+    return c;
+  };
+
+  handleChange = (event: ChangeEvent<{ value: string; id: string }>) => {
+    let id = event.target.id.split(",");
+    console.log(id);
+    let x: number = Number(id[0]);
+    let y: number = Number(id[1]);
+    let content: ICoordinate[][] = this.state.content;
+    console.log(content);
+    content[x][y].val = event.target.value;
+    console.log(content);
+    this.setState({ content: content });
+  };
+
+  parseContentToCoordinates = (content: string[][]) => {
+    let coordinates: ICoordinate[][] = [];
+    content.forEach((row, x) => {
+      coordinates[x] = [];
+      row.forEach((col, y) => {
+        coordinates[x][y] = {
+          x: x,
+          y: y,
+          val: col,
+          isLocked: col !== "" && col !== "0",
+        };
+      });
+    });
+
+    return coordinates;
   };
 }
 
